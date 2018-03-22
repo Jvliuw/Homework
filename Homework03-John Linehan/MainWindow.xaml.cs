28a7653e2b93de64013db0f90812e8f86b2895de
Jvliuw <37124332+Jvliuw@users.noreply.github.com>
2018-03-22 12:55:26 -0700

    Add files via upload

---
+++ Homework03-John Linehan/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+
+//https://code.msdn.microsoft.com/windowsdesktop/Sorting-a-WPF-ListView-by-5769086f
+
+namespace Homework03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private ListSortDirection _sortDirection;
+        private GridViewColumnHeader _sortColumn;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            uxList.ItemsSource = UserList();
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            view.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name", System.ComponentModel.ListSortDirection.Ascending));
+            view.SortDescriptions.Add(new System.ComponentModel.SortDescription("Password", System.ComponentModel.ListSortDirection.Ascending));
+        }
+
+        private void uxListClick(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = e.OriginalSource as GridViewColumnHeader;
+            if (column == null) return;
+
+            if (_sortColumn == column)
+            {
+                _sortDirection = _sortDirection == ListSortDirection.Ascending ?
+                                     ListSortDirection.Descending :
+                                     ListSortDirection.Ascending;
+            }
+            else
+            {
+                if (_sortColumn != null)
+                {
+                    _sortColumn.Column.HeaderTemplate = null;
+                    _sortColumn.Column.Width = _sortColumn.ActualWidth - 20;
+                }
+
+                _sortColumn = column;
+                _sortDirection = ListSortDirection.Ascending;
+                column.Column.Width = column.ActualWidth + 20;
+            }
+
+            string header = string.Empty;
+
+            Binding b = _sortColumn.Column.DisplayMemberBinding as Binding;
+            if(b != null)
+            {
+                header = b.Path.Path;
+            }
+
+            ICollectionView resultDataView = CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            resultDataView.SortDescriptions.Clear();
+            resultDataView.SortDescriptions.Add(new SortDescription(header, _sortDirection));
+        }
+        private List<User> UserList()
+        {
+            List<User> users = new List<User>();
+
+            users.Add(new User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new User { Name = "Lisa", Password = "3LisaPwd" });
+
+            return users;
+        }
+
+
+    }
+}
---
+++ Homework03-John Linehan/User.cs
@@ -0,0 +1,179 @@
+﻿using System.ComponentModel;
+
+namespace Homework03
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+
+        private string nameError;
+
+        //string override
+        public override string ToString()
+        {
+            return name;
+        }
+
+        private string passwordError;
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string PasswordError
+        {
+            get
+            {
+                return PasswordError;
+            }
+            set
+            {
+                if (PasswordError != value)
+                {
+                    PasswordError = value;
+                    OnPropertyChanged("PasswordError");
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return NameError;
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                NameError = "";
+                PasswordError = "";
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+
+                            return NameError;
+                        }
+                    case "Password":
+                        {
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Password cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Password cannot be longer than 12 characters.";
+                            }
+                            return NameError;
+                        }
+                }
+                return string.Empty;
+            }
+        }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+//The IDataErrorInfo interface requires the Error property and the this[string columnName] method. This is used provide support for ValidatesOnDataErrors=True in the XAML below.
+
+public interface IDataErrorInfo
+{
+    //
+    // Summary:
+    //     Gets the error message for the property with the given name.
+    //
+    // Parameters:
+    //   columnName:
+    //     The name of the property whose error message to get.
+    //
+    // Returns:
+    //     The error message for the property. The default is an empty string ("").
+    string this[string columnName] { get; }
+
+    //
+    // Summary:
+    //     Gets an error message indicating what is wrong with this object.
+    //
+    // Returns:
+    //     An error message indicating what is wrong with this object. The default is an
+    //     empty string ("").
+    string Error { get; }
+
+
+}
+
+// Summary:
+//     Notifies clients that a property value has changed.
+public interface INotifyPropertyChanged
+{
+    //
+    // Summary:
+    //     Occurs when a property value changes.
+    event PropertyChangedEventHandler PropertyChanged;
+
+}
+
